537a2d0904a659b8d43aca9a9583d446a7db5e1e
alphacrat <bismoydas333@gmail.com>
2025-02-02 12:10:24 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://movie-recommendation-backend-2opk.onrender.com'],
         credentials: true
     }
 ))
